e9132a4fbd2d50b3d0b85af8e26bb471a4435148
midjeremy <152017011+midjeremy@users.noreply.github.com>
2025-06-08 23:58:41 -0400

    Se agrega .txt para acordar comandos

--- APP/login.py
+++ APP/login.py
@@ -1,4 +1,7 @@
 from kivy.uix.screenmanager import Screen
+from db import get_db
+
+dataBase = get_db()
 
 class Login(Screen):
     """
@@ -6,3 +9,42 @@ class Login(Screen):
     This class represents a screen in the Kivy application.
     """
     pass
+
+def validate_user(username, password):
+    """
+    VALIDACION DE USUARIO RESPECTIVO A LA BASE DE DATOS Y COLECCION
+
+    :parametro usuario camionero
+    :parametro contrasena camionero
+
+    :return: True si el usuario y la contrasena son correctos, False en caso contrario
+
+    Esta funcion valida si el usuario y la contrasena son correctos
+    SE VUELVE A BUSCAR EN LA BASE DE DATOS PARA VERIFICAR SI EL USUARIO EXISTE
+
+    :parametro username: Nombre de usuario del camionero
+    :parametro password: Contrasena del camionero
+    :return: True si el usuario y la contrasena son correctos, False en caso contrario
+
+    SI NO SE ENCUENTRA EL USUARIO O LA CONTRASENA NO ES CORRECTA, SE RETORNA False;
+
+    SE BUSCA USUARIO ADMIN EN LA BASE DE DATOS
+
+    :parametro usuario administrador
+    :parametro contrasena administrador
+
+    :return: True si el usuario y la contrasena son correctos, False en caso contrario
+
+    Esta funcion valida si el usuario y la contrasena son correctos
+    SE VUELVE A BUSCAR EN LA BASE DE DATOS PARA VERIFICAR SI EL USUARIO EXISTE
+
+    :parametro username: Nombre de usuario del administrador
+    :parametro password: Contrasena del administrador
+    :return: True si el usuario y la contrasena son correctos, False en caso contrario
+
+    """
+    '''usuario = dataBase['Usuarios_Camioneros'].fingd_one({
+        'username': username,'''
+    
+    user = None
+    return user is not None
--- db.py
+++ db.py
@@ -1,5 +1,14 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-def get_db ():
-    cliente = MongoClient["url"]
-    return cliente["db"]
+# Cargar variables del archivo .env
+load_dotenv()
+
+def get_db():
+    mongo_url = os.getenv("MONGO_URL")
+    db_name = os.getenv("MONGO_DB")
+    
+    cliente = MongoClient(mongo_url)
+    return cliente[db_name]
+
