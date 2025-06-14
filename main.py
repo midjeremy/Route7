8a7306a4b40979c3a6aebc5f9e5a6f09429f8cc8
Camilo-rgb <camilovega946@gmail.com>
2025-06-14 01:13:29 -0400

    se actualizo los colores del admin

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from kivy.uix.screenmanager import ScreenManager
 #importar pantallas
 from APP.login import LoginScreen
 from APP.admin import AdminScreen
-from APP.mapa import MapScreen
-from APP.user import UserScreen
 
 class MyScreenManager(ScreenManager):
     pass
@@ -17,13 +15,10 @@ class MiApp(App):
         from kivy.lang import Builder
         Builder.load_file('template/login.kv')
         Builder.load_file('template/admin.kv')
-        Builder.load_file('template/map.kv')
 
         sm = MyScreenManager()
         sm.add_widget(LoginScreen(name='login'))
         sm.add_widget(AdminScreen(name='admin'))
-        sm.add_widget(MapScreen(name='mapa'))
-        sm.add_widget(UserScreen(name='user'))
         return sm
     
 if __name__ == '__main__':
